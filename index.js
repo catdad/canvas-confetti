7ada7817f9d68426aba64b9e1f71f48179fbc5f4
Kiril Vatev <vatev.1@gmail.com>
2018-01-27 17:00:50 -0500

    adding origin.x and origin.y APIs

--- index.js
+++ index.js
@@ -19,6 +19,8 @@
     startVelocity: 45,
     decay: 0.9,
     ticks: 200,
+    x: 0.5,
+    y: 0.5,
     colors: [
       '#26ccff',
       '#a25afd',
@@ -63,6 +65,14 @@
       g: toDecimal(val.substring(2,4)),
       b: toDecimal(val.substring(4,6))
     };
+  }
+
+  function getOrigin(options) {
+    var origin = prop(options, 'origin', Object);
+    origin.x = prop(origin, 'x', Number);
+    origin.y = prop(origin, 'y', Number);
+
+    return origin;
   }
 
   function getCanvas(zIndex) {
@@ -176,16 +186,20 @@
     var decay = prop(options, 'decay', Number);
     var colors = prop(options, 'colors');
     var ticks = prop(options, 'ticks', Number);
+    var origin = getOrigin(options);
 
     var temp = particleCount;
     var fettis = [];
     var canvas = animationObj ? animationObj.canvas : getCanvas(options ? options.zIndex : null);
 
+    var startX = canvas.width * origin.x;
+    var startY = canvas.height * origin.y;
+
     while (temp--) {
       fettis.push(
         randomPhysics({
-          x: canvas.width / 2,
-          y: canvas.height / 2,
+          x: startX,
+          y: startY,
           angle: angle,
           spread: spread,
           startVelocity: startVelocity,
