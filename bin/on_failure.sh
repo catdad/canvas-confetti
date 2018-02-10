d7ccdd4f26ad9ecfc61f12b70047ff57a762655a
Kiril Vatev <vatev.1@gmail.com>
2018-02-09 20:08:37 -0500

    adding a bash script that uploads images to transfer.sh

---
+++ bin/on_failure.sh
@@ -0,0 +1,18 @@
+upload_file () {
+  filename=$1
+  urlname=${filename// /_}
+
+  downloadurl=`curl --upload-file "./$filename" https://transfer.sh/$urlname`
+
+  echo image \"$filename\"
+  echo "  uploaded to: $downloadurl"
+  echo --------------------------------------
+}
+
+find_files () {
+  cd shots
+
+  for i in *.png;do upload_file "$i";done
+}
+
+find_files
