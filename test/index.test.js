d8c0c610721c9fab713df4d233f5ef6d9bd78599
Kiril Vatev <vatev.1@gmail.com>
2018-02-23 09:47:25 -0500

    allowing 7 or more colors in the default confetti test

--- test/index.test.js
+++ test/index.test.js
@@ -228,7 +228,7 @@ test('shoots default confetti', async t => {
 
   const pixels = await uniqueColors(t.context.image);
 
-  t.is(pixels.length, 8);
+  t.true(pixels.length >= 7);
 });
 
 test('shoots red confetti', async t => {
